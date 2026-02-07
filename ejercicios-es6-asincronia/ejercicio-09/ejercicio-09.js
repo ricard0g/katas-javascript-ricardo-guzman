e671fd353048cefa5fd5bc25d6d2e68c6094e3fe
ricard0g <ricardoguzman444@gmail.com>
2026-02-07 12:30:31 +0100

    feat: add HTML structure and JavaScript for random Pokémon image fetching

---
+++ ejercicios-es6-asincronia/ejercicio-09/ejercicio-09.js
@@ -0,0 +1,36 @@
+const img = document.querySelector(".random-image");
+
+// Opcion 1
+
+fetch(
+	`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 152) + 1}`,
+)
+	.then((res) => res.json())
+	.then((pokemon) => {
+		img.src = pokemon.sprites.front_default;
+	})
+	.catch((error) =>
+		console.log("Error en la extraccion del Pokemon: " + error),
+	);
+
+// Opcion 2 Async Await
+
+// const pokemonFetch = async () => {
+// 	try {
+// 		const response = await fetch(
+// 			`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 152) + 1}`,
+// 		);
+
+// 		if (!response) {
+// 			throw new Error("Error en la extraccion del Pokemon: " + response);
+// 		}
+
+// 		const pokemon = await response.json();
+
+// 		img.src = pokemon.sprites.front_default;
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
+
+// pokemonFetch();
